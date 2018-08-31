8d3cb1d1ecd0592b133d648753e3c3ed32b2a93f
Orbelio Filho <orbelio@gmail.com>
2018-08-31 15:07:37 -0300

    Estudo finalizado.

--- contas/models.py
+++ contas/models.py
@@ -8,8 +8,9 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
+
 class Transacao(models.Model):
-    data = models.DateTimeField(auto_now_add=True)
+    data = models.DateTimeField()
     descricao = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
--- contas/views.py
+++ contas/views.py
@@ -1,17 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
 
+from .form import TransacaoForm
 from .models import Transacao
+
 
 def home(request):
     data = {}
     data['transacoes'] = ['t1', 't2', 't3']
     data['now'] = datetime.now()
-    #html = "<html><body>Agora é %s.</body></html>" % now
-    #return HttpResponse(html)
+    # html = "<html><body>Agora é %s.</body></html>" % now
+    # return HttpResponse(html)
     return render(request, 'contas\home.html', data)
+
 
 def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request, 'contas\listagem.html', data)
+
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
+    
